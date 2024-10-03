d72507f9b67aaa9b2fb07628f1772468a0ac7881
Youssef Labouiti <labouiti.youssef@gmail.com>
2024-10-03 15:44:26 +0200

    fin

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -7,34 +7,35 @@ const projects = [
     id: 1,
     title: "OhMyFood",
     description: "Un site “mobile first” qui répertorie les menus de restaurants",
-    image: "/image/Ohmyfood.jpg",
+    image: `${process.env.PUBLIC_URL}/image/Ohmyfood.jpg`,
     lien: "https://labouitiyou.github.io/Projet-Site-d-un-restaurant/"
   },
   {
     id: 2,
     title: "Booki",
     description: "Un site d'une agence de voyage",
-    image: "/image/booki.png",
+    image: `${process.env.PUBLIC_URL}/image/booki.png`,
     lien: "https://labouitiyou.github.io/Projet_Agence-de-voyage/"
   },
   {
     id: 3,
     title: "ArgentBank",
     description: "Une application bancaire avec React",
-    image: "/image/argentbank.jpg",
+    image: `${process.env.PUBLIC_URL}/image/argentbank.jpg`,
     lien: "https://github.com/Labouitiyou/ArgentBank-website.git"
   },
   {
     id: 4,
     title: "Kasa",
     description: "Une application web de location immobilière avec React",
-    image: "/image/kasa.jpg" 
+    image: `${process.env.PUBLIC_URL}/image/kasa.jpg`,
+    lien: "https://github.com/Labouitiyou/Application-web-kasa-React-.git"
   },
   {
     id: 5,
     title: "Sophie Bluel",
     description: "Un site portfolio d’une architecte",
-    image: "/image/sophie.jpg",
+    image: `${process.env.PUBLIC_URL}/image/sophie.jpg`,
     lien: "https://github.com/Labouitiyou/Portfolio-architecte-sophie-bluel.git"
 
   },
@@ -42,21 +43,21 @@ const projects = [
     id: 6,
     title: "Nina Carducci",
     description: "Optimiser le référencement d'un site de photographe",
-    image: "/image/nina.jpg",
+    image: `${process.env.PUBLIC_URL}/image/nina.jpg`,
     lien : "https://labouitiyou.github.io/ninacarducci.github.io/"
   },
   {
     id: 7,
     title: "724events",
     description: "Débugger le site d'une agence d'événementiel",
-    image: "/image/events.jpg",
+    image: `${process.env.PUBLIC_URL}/image/events.jpg`,
     lien : "https://github.com/Labouitiyou/Debuggez-une-application-React.JS.git"
   },
   {
-    id: 7,
+    id: 8,
     title: "Print It",
     description: "Un site dynamique d'une imprimerie",
-    image: "/image/print.jpg",
+    image: `${process.env.PUBLIC_URL}/image/print.jpg`,
     lien : "https://labouitiyou.github.io/Print-it-JS/"
   }
 ];
